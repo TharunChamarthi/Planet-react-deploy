6b785d67a4b2b337d214cd1223b477133332edff
TharunChamarthi <ch.tharunkumar@gmail.com>
2020-12-27 22:17:33 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import API from './utils/API';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
+
+  const [planets, setPlanets] = useState([]);
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      const res = await API.get('planet');
+      setPlanets(res.data)
+    } catch (err) {
+      console.log(err);
+      console.log(err.message);
+    }
+  }
+
+  const changeFav = (id) => {
+    const changePlanets = [...planets]
+    const planet = changePlanets.find(planet => planet.id === id);
+    planet.isFavourite = !planet.isFavourite;
+    setPlanets(changePlanets);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
+      <TabList>
+        <Tab>Planets</Tab>
+        <Tab>Favorite planets</Tab>
+      </TabList>
+      <TabPanel>
+        <ul>
+          {planets.map((planet) => {
+            return (
+              <li key={planet.id}><p>{planet.name}</p> <span onClick={() => changeFav(planet.id)} className={`material-icons ${planet.isFavourite ? `fav` : `not-fav`}`}>
+                favorite
+              </span></li>
+            )
+          })}
+        </ul>
+      </TabPanel>
+      <TabPanel><ul>
+        {planets.filter(planet => planet.isFavourite).map((planet) => {
+          return (
+            <li key={planet.id}><p>{planet.name}</p> <span onClick={() => changeFav(planet.id)} className={`material-icons ${planet.isFavourite ? `fav` : `not-fav`}`}>
+              favorite
+              </span></li>
+          )
+        })}
+      </ul></TabPanel>
+    </Tabs>
   );
 }
 
